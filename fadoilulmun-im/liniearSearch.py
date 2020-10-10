e9a3454971c751ab3619a40b745a475649e8eea3
Fadoilul Munim <fadoilulmun.im2807@gmail.com>
2020-10-10 20:53:49 +0700

    Menambah algoritma pencarian linier

---
+++ fadoilulmun-im/liniearSearch.py
@@ -0,0 +1,16 @@
+
+def linierSearch(arr, n, x):
+
+    for i in range(0, n):
+        if arr[i] == x:
+            return i
+    return False
+
+
+arr = [1, 3, 6, 7, 10, 90]
+ygdicari = 90
+hasil = linierSearch(arr, len(arr), ygdicari)
+if hasil:
+    print("Elemen di temukan di index ke", hasil)
+else:
+    print("Elemen yang dicari tidak ada dalam array")
